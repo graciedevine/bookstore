9e77648289dbb7591dc12438b39a6abb0e3a8827
graciedevine <gracie.devine@gmail.com>
2021-05-04 08:59:32 -0400

    Changed margins in Contact Us image to make it responsive with img-fluid tag.

--- bookstore/wsgi.py
+++ bookstore/wsgi.py
@@ -1,4 +1,3 @@
-<<<<<<< HEAD
 """
 WSGI config for bookstore project.
 
@@ -15,22 +14,3 @@ from django.core.wsgi import get_wsgi_application
 os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'bookstore.settings')
 
 application = get_wsgi_application()
-=======
-"""
-WSGI config for bookstore project.
-
-It exposes the WSGI callable as a module-level variable named ``application``.
-
-For more information on this file, see
-https://docs.djangoproject.com/en/3.1/howto/deployment/wsgi/
-"""
-
-import os
-
-from django.core.wsgi import get_wsgi_application
-
-os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'bookstore.settings')
-
-application = get_wsgi_application()
->>>>>>> e4c94b290725799df214252e277437267a41de12
-
