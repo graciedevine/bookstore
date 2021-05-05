7c77520427a7dbbb9b37bc82e29b6e78a74ca923
graciedevine <gracie.devine@gmail.com>
2021-05-05 09:17:20 -0400

    Debug features set back to default.

--- bookstore/settings.py
+++ bookstore/settings.py
@@ -12,7 +12,7 @@ https://docs.djangoproject.com/en/3.1/ref/settings/
 
 from pathlib import Path
 
-# from top_secret import the_key
+from top_secret import the_key
 import os
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
@@ -23,8 +23,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/3.1/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-# SECRET_KEY = the_key
-SECRET_KEY = "e8szx4^kck_gzkv5n8q%a1b6yix=#0txmii48xgq+=d%(hlo%3"
+SECRET_KEY = the_key
 
 
 # SECURITY WARNING: don't run with debug turned on in production!
