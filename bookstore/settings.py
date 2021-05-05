bd820fe387cd1aea960088be23f35fc46399fc30
graciedevine <gracie.devine@gmail.com>
2021-05-05 08:43:48 -0400

    Set DEBUG based on environment variables.

--- bookstore/settings.py
+++ bookstore/settings.py
@@ -138,3 +138,8 @@ STATIC_ROOT = os.path.join(BASE_DIR, "staticfiles")
 import django_heroku
 
 django_heroku.settings(locals())
+
+if os.environ.get("DEBUG") == "TRUE":
+    DEBUG = True
+elif os.environ.get("DEBUG") == "FALSE":
+    DEBUG = False
