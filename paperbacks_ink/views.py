383431ed58bbf3909dc7bb01739833fa6a6d9c33
graciedevine <gracie.devine@gmail.com>
2021-05-05 09:06:46 -0400

    Testing 404 error page.

--- paperbacks_ink/views.py
+++ paperbacks_ink/views.py
@@ -1,17 +1,22 @@
-from django.shortcuts import render
+from django.shortcuts import render, redirect, get_object_or_404
+
 
 def index(request):
     """The home page for Paperbacks Ink."""
-    return render(request, 'paperbacks_ink/index.html')
+    error = get_object_or_404()
+    return render(request, "paperbacks_ink/index.html")
+
 
 def events(request):
     """Shows Events page."""
-    return render(request, 'paperbacks_ink/events.html')
+    return render(request, "paperbacks_ink/events.html")
+
 
 def kaffee(request):
     """Shows Kaffee Kultur page."""
-    return render(request, 'paperbacks_ink/kaffee.html')
+    return render(request, "paperbacks_ink/kaffee.html")
+
 
 def contact(request):
     """Shows Contact Us page."""
-    return render(request, 'paperbacks_ink/contact.html')
+    return render(request, "paperbacks_ink/contact.html")
